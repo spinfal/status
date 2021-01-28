b863fdad2dc2736ade6ba65fefd0f0f121d0fcfb
spinfal <spinfal@atheist.com>
2021-01-28 09:41:47 -0800

    fixed indentation and added user-agent
    i didnt do this like 3 times shut up

--- script.js
+++ script.js
@@ -32,7 +32,7 @@
       
       stat.setRequestHeader('Content-type', 'application/json');
       stat.setRequestHeader('Authorization', token);
-			stat.setRequestHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 OPR/72.0.3815.473')
+      stat.setRequestHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 OPR/72.0.3815.473')
       
       if (emote==="") {
         emote = null;
